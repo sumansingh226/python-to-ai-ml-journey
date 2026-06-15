398061df34597eba496bb6ab849decb3a8a47b1e
Suman Singh <sumansingh13062000@gmail.com>
2026-06-15 23:49:17 +0530

    Day 1: Python basics (print, input, variables)

--- src/basic/day1_print_input.py
+++ src/basic/day1_print_input.py
@@ -1,2 +1,7 @@
-name = input("Enter your name..")
-print("welecom",name, "into Ai/Ml world with python...")
+# input() is used to take input from the user (keyboard)
+# It always returns the value as a string (text)
+name = input("Enter your name: ")
+
+# print() is used to display output on the screen
+# Here we are combining text + variable (name) to make a message
+print("Welcome", name, "to AI/ML journey 🚀")
