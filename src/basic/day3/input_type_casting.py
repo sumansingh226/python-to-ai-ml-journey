c5baf0fecdb9f855b009e9119448c9dcaafaeb69
Suman Singh <sumansingh13062000@gmail.com>
2026-06-17 23:32:35 +0530

    type casting

--- src/basic/day3/input_type_casting.py
+++ src/basic/day3/input_type_casting.py
@@ -30,3 +30,58 @@ bool()  -> Convert to Boolean
 """
 
 #  BASIC INPUT
+name = input("Enter your name ")
+print("\n Basic input example:")
+print(f"Hello,{name}!")
+print(type(name)) #input() return string
+print()
+
+#input of numbers 
+age = input("Enter your gae :")
+print("\n Age Example : ")
+print(age)
+print(type(age)) #still string
+print()
+
+#string to integer
+
+age = int(age)
+print("String to integer")
+print(type(age))
+print()
+
+#string to float 
+
+height = float(input("Enter your height : "))
+print("\n string to float:")
+print(type(height))
+print()
+
+#  INTEGER TO STRING
+marks = 95
+
+marks_str = str(marks)
+
+print("Integer to String:")
+print(marks_str)
+print(type(marks_str))
+print()
+
+# FLOAT TO INTEGER
+price = 99.99
+
+price_int = int(price)
+
+print("Float to Integer:")
+print(price_int)
+print(type(price_int))
+print()
+
+#BOOLEAN CONVERSION
+
+print("Boolean Conversion:")
+
+print(bool(1))      # True
+print(bool(0))      # False
+print(bool("Hi"))   # True
+print(bool(""))     # False
