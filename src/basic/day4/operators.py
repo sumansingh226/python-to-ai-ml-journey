abb9bd28a0d1ce0ca6cf589a7ec40f9ff202cbf3
Suman Singh <sumansingh13062000@gmail.com>
2026-06-19 23:20:23 +0530

    operators in py

---
+++ src/basic/day4/operators.py
@@ -0,0 +1,27 @@
+"""
+=========================================================
+OPERATORS IN PYTHON
+=========================================================
+
+What are Operators?
+-------------------
+Operators are symbols used to perform operations on
+variables and values.
+
+Why Do We Use Operators?
+------------------------
+1. To perform calculations.
+2. To compare values.
+3. To make decisions in programs.
+4. To combine conditions.
+5. To update variable values.
+
+Types of Operators:
+-------------------
+1. Arithmetic Operators
+2. Comparison Operators
+3. Logical Operators
+4. Assignment Operators
+5. Membership Operators
+6. Identity Operators
+"""
