768e685106028428066ebc4999c8f5f96b05d696
Suman Singh <sumansingh13062000@gmail.com>
2026-06-20 00:17:18 +0530

    elseif

--- src/basic/day4/conditional_statements.py
+++ src/basic/day4/conditional_statements.py
@@ -11,3 +11,30 @@ Why do we use them?
 - To execute code only when conditions are met
 """
 
+
+#if statement 
+
+age = 20
+if age>=18:
+   print("You are eligible for vote")
+
+#if-else 
+age = 16
+if age>=18:
+   print("you can vote.")
+else :
+   print("you can't vote")
+
+#if-else-if
+marks = 65
+if marks >= 90:
+   print("A+")
+elif marks >= 75:
+   print("A")
+elif marks >= 60:
+   print("B")
+elif marks >= 40:
+   print("C")
+else:
+   print("failed")
+    
