9ed2763d7ef343680355e26081b55ebeca4b8ba5
Suman Singh <sumansingh13062000@gmail.com>
2026-06-20 00:06:48 +0530

    conditional statemnets

---
+++ src/basic/day4/conditional_statements.py
@@ -0,0 +1,13 @@
+"""
+Conditional Statements in Python
+
+What are Conditional Statements?
+Conditional statements allow a program to make decisions
+based on conditions.
+
+Why do we use them?
+- To make decisions
+- To control program flow
+- To execute code only when conditions are met
+"""
+
