7d402d7d4652a9129eb9668cfc595652d192f45b
Suman Singh <sumansingh13062000@gmail.com>
2026-06-23 00:04:00 +0530

    Copying Lists

--- src/basic/day6/lists.py
+++ src/basic/day6/lists.py
@@ -26,3 +26,88 @@ Why do we use Lists?
 # clear()    -> Remove all items
 # count()    -> Count occurrences
 # index()    -> Find position of item
+
+
+#creating a list 
+
+skills = ["Python", "Sql", "Machine learning"]
+numbers = [10,20,30,40]
+mixed = ["Rahul",25,True,5.9]
+
+print(skills)
+print(numbers)
+print(mixed)
+
+# Accessing Elements (Indexing)
+
+print(skills[0])
+print(skills[1])
+print(skills[-1])
+
+# Slicing
+
+print(numbers[0:2])
+print(numbers[1::3])
+
+
+
+# Length of List
+
+print(len(skills))
+
+
+#adding elements 
+
+skills.append("JavaScript")
+print(skills)
+
+#inserting elements 
+
+skills.insert(1,"Git")
+print(skills)
+
+# Removing Elements
+
+skills.remove("Sql")
+print(skills)
+
+# Remove Last Element
+skills.pop()
+print(skills)
+
+# Updating Elements
+
+skills[0] = "Python Programming"
+
+print(skills)
+
+# Checking Existence
+
+print("Git" in skills)
+
+# Looping Through a List
+
+for skill in skills:
+    print(skill)
+
+# Sorting
+
+marks = [90, 70, 85, 60]
+print("marks before sort : ",marks)
+marks.sort()
+print("sort ",marks)
+
+# Reverse Sorting
+
+marks.sort(reverse=True)
+
+print(marks)
+
+
+# Copying Lists
+
+list1 = [1, 2, 3]
+
+list2 = list1.copy()
+
+print(list2)
