858a5780ce0bbeff1258f8db95a5ffd253b6e847
Suman Singh <sumansingh13062000@gmail.com>
2026-06-22 22:34:05 +0530

    strings in python

---
+++ src/basic/day5/strings.py
@@ -0,0 +1,19 @@
+
+
+#strings in python 
+
+"""
+Strings in Python
+
+What is a String?
+A string is a sequence of characters enclosed in
+single quotes ('') or double quotes ("").
+
+Why do we use Strings?
+- Store text data
+- User names
+- Messages
+- File names
+- Data processing
+- NLP and AI applications
+"""
