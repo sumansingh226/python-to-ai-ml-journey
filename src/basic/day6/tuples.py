566f7329d5d23423c69e0cde167668452988a2cb
Suman Singh <sumansingh13062000@gmail.com>
2026-06-23 00:11:16 +0530

    Auto commit

---
+++ src/basic/day6/tuples.py
@@ -0,0 +1,22 @@
+"""
+Tuples in Python
+
+What is a Tuple?
+A tuple is an ordered, immutable (cannot be changed)
+collection of items.
+
+Think of a tuple as a list whose contents should remain fixed.
+
+Example:
+
+coordinates = (10, 20)
+
+Why Do We Use Tuples?
+
+1. Store Multiple Values
+------------------------
+Tuples can hold multiple related values in a single variable.
+
+"""
+
+
