5f51fbf4d76c299c0debcc123710964a5c0c74b2
Suman Singh <sumansingh13062000@gmail.com>
2026-06-22 22:20:53 +0530

    Auto commit

---
+++ src/basic/day5/loops.py
@@ -0,0 +1,22 @@
+"""
+Loops in Python
+
+What are Loops?
+Loops are used to execute a block of code repeatedly.
+
+Why do we use Loops?
+- To avoid writing repetitive code
+- To process collections of data
+- To automate repetitive tasks
+- To iterate through lists, strings, files, etc.
+
+Types of Loops:
+1. for loop
+2. while loop
+3. nested loops
+4. break
+5. continue
+6. pass
+7. range()
+"""
+
