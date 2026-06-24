074909aa29a555cf2aece82cba3e4817c6e97214
Suman Singh <sumansingh13062000@gmail.com>
2026-06-25 01:28:44 +0530

    Function Calling Another Function

--- src/basic/day8/package.py
+++ src/basic/day8/package.py
@@ -1,3 +1,33 @@
-# What is a Package?
+"""
+PACKAGES IN PYTHON
 
-# A package is a folder that contains multiple Python modules.
+What is a Package?
+
+A package is a folder that contains one or more Python modules.
+
+Module  = Python file (.py)
+Package = Folder containing Python files
+
+Why do we use Packages?
+
+1. Organize code
+2. Group related modules together
+3. Avoid huge files
+4. Improve project structure
+5. Make code reusable
+
+Real Life Example
+
+Imagine an AI project:
+
+ai_project/
+
+├── data/
+├── models/
+├── utils/
+├── training/
+└── main.py
+
+Instead of putting 5000 lines in one file,
+we split code into packages and modules.
+"""
