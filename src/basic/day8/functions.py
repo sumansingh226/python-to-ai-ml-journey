79d611b004a98c8ceb78fd4b248ce73cabba3447
Suman Singh <sumansingh13062000@gmail.com>
2026-06-24 23:05:53 +0530

    Simple Function

---
+++ src/basic/day8/functions.py
@@ -0,0 +1,29 @@
+"""
+Functions in Python
+
+What is a Function?
+
+A function is a reusable block of code that performs
+a specific task.
+
+Why do we use Functions?
+
+1. Avoid code repetition
+2. Improve code readability
+3. Make code reusable
+4. Break large programs into smaller parts
+5. Easier testing and debugging
+
+Real Life Example
+
+Imagine a calculator.
+
+Instead of writing addition logic every time,
+we create one function and reuse it whenever needed.
+"""
+
+# Simple Function
+def hello():
+    print("hello from simple function")
+
+hello()
