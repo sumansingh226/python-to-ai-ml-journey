73afc719a6d185634cdfda55a6328497032b1743
Suman Singh <sumansingh13062000@gmail.com>
2026-06-25 00:29:05 +0530

    Function Calling Another Function

---
+++ src/basic/day7/modules_packages.py
@@ -0,0 +1,16 @@
+"""
+Modules and Packages in Python
+
+What is a Module?
+
+A module is a Python file containing code
+(functions, variables, classes) that can be reused
+in other Python files.
+
+Why do we use Modules?
+
+- Reuse code
+- Organize projects
+- Avoid duplication
+- Use Python's built-in functionality
+"""
