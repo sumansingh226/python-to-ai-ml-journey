354e691bbb33bfd1377dd88544225f508819647f
Suman Singh <sumansingh13062000@gmail.com>
2026-06-28 00:38:35 +0530

    =========================

---
+++ src/medium/day11/day16_oop_intro.py
@@ -0,0 +1,31 @@
+#oops in python 
+"""
+Object-Oriented Programming (OOP)
+
+What is OOP?
+
+Object-Oriented Programming is a programming paradigm
+that organizes code using objects and classes.
+
+Instead of writing everything in functions,
+we create objects that contain both data and behavior.
+
+Why do we use OOP?
+
+1. Better code organization
+2. Code reusability
+3. Easier maintenance
+4. Real-world modeling
+5. Used in large applications
+
+Real-Life Example
+
+Class = Car Blueprint
+
+Object = BMW
+Object = Audi
+Object = Tesla
+
+All cars share common features,
+but each has different values.
+"""
