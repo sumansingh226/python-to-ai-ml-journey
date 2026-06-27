0c6a496ad5c4464c8e7990a7ec8019a7303839ac
Suman Singh <sumansingh13062000@gmail.com>
2026-06-27 22:04:10 +0530

    =========================

---
+++ src/basic/day10/file_handling.py
@@ -0,0 +1,22 @@
+#file handling in python
+"""
+File Handling in Python
+
+What is File Handling?
+
+File handling is the process of creating,
+reading, writing, updating, and deleting files.
+
+Why do we use File Handling?
+
+1. Store data permanently
+2. Read data from files
+3. Save logs
+4. Save user information
+5. Read datasets
+6. Process CSV and text files
+
+Without file handling,
+all program data is lost after the program ends.
+"""
+
