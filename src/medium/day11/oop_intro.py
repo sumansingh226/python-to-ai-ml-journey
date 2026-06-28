3c4e9a7d2934ee0f7c75015162f69093a2e075b4
Suman Singh <sumansingh13062000@gmail.com>
2026-06-28 22:39:08 +0530

    calling methods

--- src/medium/day11/oop_intro.py
+++ src/medium/day11/oop_intro.py
@@ -31,3 +31,19 @@ but each has different values.
 """
 
 
+#create a class in python 
+
+class Student:
+    #method
+    def greet(self):
+        print("Welcome to Python class")
+
+#creating objects
+
+student1 = Student()
+student2 = Student()
+
+#calling methods 
+
+student1.greet()
+student2.greet()
