4c00aeb54ff21c6b1c14873e9b0271ec4e2e70f3
Suman Singh <sumansingh13062000@gmail.com>
2026-06-30 00:18:41 +0530

    constructirs in python

---
+++ src/medium/day12/constructor.py
@@ -0,0 +1,31 @@
+"""
+Constructors (__init__) in Python
+
+What is a Constructor?
+
+A constructor is a special method that is
+automatically called whenever an object is created.
+
+In Python, the constructor is written as:
+
+__init__()
+
+Why do we use Constructors?
+
+1. Initialize object data
+2. Assign values to object attributes
+3. Avoid writing repetitive code
+4. Automatically prepare the object
+
+Real-Life Example
+
+Imagine creating a Student.
+
+Every student has:
+- Name
+- Age
+- Course
+
+Instead of setting these manually every time,
+the constructor initializes them automatically.
+"""
