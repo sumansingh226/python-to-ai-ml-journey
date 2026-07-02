8f73ce664a280d534af1d98be68160c025fcf848
Suman Singh <sumansingh13062000@gmail.com>
2026-07-02 22:48:04 +0530

    Instance vars vs class vars

---
+++ src/medium/day13/instance_vs_class_variables.py
@@ -0,0 +1,15 @@
+"""
+Instance Variables vs Class Variables
+
+What is an Instance Variable?
+
+An instance variable belongs to an object.
+
+Every object has its own copy.
+
+What is a Class Variable?
+
+A class variable belongs to the class.
+
+All objects share the same copy.
+"""
