d4ecb7bc554bed220ec0ebf8d077f066dec649a1
Suman Singh <sumansingh13062000@gmail.com>
2026-07-03 20:06:29 +0530

    methods in python

---
+++ src/medium/day14/methods.py
@@ -0,0 +1,13 @@
+"""
+Methods in Python
+
+What is a Method?
+
+A method is a function defined inside a class.
+
+Python has three types of methods:
+
+1. Instance Method
+2. Class Method
+3. Static Method
+"""
