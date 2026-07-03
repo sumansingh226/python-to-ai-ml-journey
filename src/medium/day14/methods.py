6e860aa4c3bad6fb72d7fed80842e2270df8c6db
Suman Singh <sumansingh13062000@gmail.com>
2026-07-03 20:07:31 +0530

    methods in python

--- src/medium/day14/methods.py
+++ src/medium/day14/methods.py
@@ -11,3 +11,45 @@ Python has three types of methods:
 2. Class Method
 3. Static Method
 """
+
+
+class Student:
+
+    # Class Variable
+    school = "ABC School"
+
+    # Constructor
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # Instance Method
+    def display(self):
+        print("Instance Method")
+        print("Name :", self.name)
+        print("Age  :", self.age)
+
+    # Class Method
+    @classmethod
+    def show_school(cls):
+        print("Class Method")
+        print("School :", cls.school)
+
+    # Static Method
+    @staticmethod
+    def welcome():
+        print("Static Method")
+        print("Welcome to Python OOP")
+
+
+student = Student("Suman", 24)
+
+student.display()
+
+print()
+
+Student.show_school()
+
+print()
+
+Student.welcome()
