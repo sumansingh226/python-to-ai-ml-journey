019b8bc179aaa7fcb1ae499c7d02e489e51a0c94
Suman Singh <sumansingh13062000@gmail.com>
2026-07-04 16:36:38 +0530

    inhticmance

---
+++ src/medium/day15/inheritance.py
@@ -0,0 +1,38 @@
+"""
+Inheritance in Python
+
+What is Inheritance?
+
+Inheritance is a feature of Object-Oriented Programming
+that allows one class (child class) to inherit the
+properties and methods of another class (parent class).
+
+Why do we use Inheritance?
+
+1. Reuse existing code.
+2. Reduce code duplication.
+3. Make programs easier to maintain.
+4. Build relationships between classes.
+
+Real-Life Example
+
+Parent
+------
+Animal
+
+Child
+-----
+Dog
+Cat
+Bird
+
+Every animal can:
+- Eat
+- Sleep
+
+A dog also:
+- Barks
+
+A cat also:
+- Meows
+"""
