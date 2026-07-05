420021357c9692cbe91b36333590b0d3c7bb6dbe
Suman Singh <79319168+sumansingh226@users.noreply.github.com>
2026-07-05 22:23:42 +0530

    Update polymorphism.py

--- src/medium/day15/polymorphism.py
+++ src/medium/day15/polymorphism.py
@@ -1,1 +1,25 @@
 """ Polymorphism in Python is an Object-Oriented Programming (OOP) concept where the same method or function can behave differently depending on the object that calls it. This allows you to write flexible and reusable code.""
+
+example 
+
+class Animal:
+    def sound(self):
+        print("Animal makes a sound")
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+
+class Cat(Animal):
+    def sound(self):
+        print("Cat meows")
+
+# Creating objects
+dog = Dog()
+cat = Cat()
+
+# Polymorphism
+dog.sound()   # Output: Dog barks
+cat.sound()   # Output: Cat meows
+
+
