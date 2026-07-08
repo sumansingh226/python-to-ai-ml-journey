ea45b82a55cf6b757d2eebd4c0c5588a9fb0abf5
Suman Singh <sumansingh13062000@gmail.com>
2026-07-08 22:17:44 +0530

    method overriding

--- src/medium/day16/method_overriding.py
+++ src/medium/day16/method_overriding.py
@@ -29,3 +29,20 @@ Every animal makes a sound,
 but each animal has its own sound.
 """
 
+#basic example 
+class Animal:
+
+    def sound(self):
+        print("Animals make sounds")
+
+
+class Dog(Animal):
+
+    def sound(self):
+        print("Dog barks")
+
+
+dog = Dog()
+
+dog.sound()
+
