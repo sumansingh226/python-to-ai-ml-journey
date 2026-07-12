822200920fcb5cd45a7597458acd4d9df14221c9
Suman Singh <sumansingh13062000@gmail.com>
2026-07-12 22:27:59 +0530

    Magic (Dunder) Methods in Python

---
+++ src/basic/day18/agic_methods.py
@@ -0,0 +1,29 @@
+"""
+Magic (Dunder) Methods in Python
+
+What are Magic Methods?
+
+Magic methods are special methods in Python that
+begin and end with double underscores (__).
+
+They allow Python objects to work with built-in
+operations like printing, adding, comparing, and more.
+
+"Dunder" stands for "Double UNDERscore."
+
+Examples:
+
+__init__()
+__str__()
+__repr__()
+__len__()
+__add__()
+__eq__()
+
+Why do we use Magic Methods?
+
+1. Customize object behavior.
+2. Work with Python built-in functions.
+3. Make objects behave like built-in types.
+4. Improve readability.
+"""
