20a41d2ef4ee8a210dfa2576e4eb96bcf7db03fb
Suman Singh <sumansingh13062000@gmail.com>
2026-07-17 21:58:39 +0530

    genrators

---
+++ src/advanced/day18/generators.py
@@ -0,0 +1,43 @@
+"""
+Generators in Python
+
+What is a Generator?
+
+A generator is a special type of function
+that returns one value at a time instead
+of returning all values at once.
+
+Generators use the keyword:
+
+yield
+
+instead of:
+
+return
+
+--------------------------------------------
+
+Why do we use Generators?
+
+1. Save memory.
+2. Process large data.
+3. Produce values one by one.
+4. Create iterators easily.
+5. Used heavily in AI/ML.
+
+--------------------------------------------
+
+return vs yield
+
+return
+-------
+Returns one value.
+Function ends.
+
+yield
+------
+Returns one value.
+Function pauses.
+Continues from where it stopped.
+"""
+
