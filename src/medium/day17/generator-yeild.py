3f53321ab0da1352ee4504187ca35b9e0e35cbb4
Suman Singh <79319168+sumansingh226@users.noreply.github.com>
2026-07-18 21:08:19 +0530

    Create generator-yeild.py

---
+++ src/medium/day17/generator-yeild.py
@@ -0,0 +1,33 @@
+"""
+Generators in Python
+
+What is a Generator?
+
+A generator is a special type of function
+that returns one value at a time instead
+of returning all values at once.
+
+Generators use the 'yield' keyword.
+
+------------------------------------------------
+
+Why do we use Generators?
+
+1. Save memory.
+2. Process large datasets.
+3. Produce values lazily (on demand).
+4. Improve performance.
+5. Used heavily in AI/ML and Data Science.
+
+------------------------------------------------
+
+Difference
+
+Function
+---------
+Returns all at once.
+
+Generator
+----------
+Returns one value at a time.
+"""
