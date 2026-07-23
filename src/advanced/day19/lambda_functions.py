aae2e4aa72875fd66314aa4ac18f99e82c9c4ad6
Suman Singh <sumansingh13062000@gmail.com>
2026-07-23 16:30:34 +0530

    lamda fun

---
+++ src/advanced/day19/lambda_functions.py
@@ -0,0 +1,37 @@
+"""
+Lambda Functions in Python
+
+What is a Lambda Function?
+
+A lambda function is a small anonymous
+(unnamed) function.
+
+It can take any number of arguments
+but contains only one expression.
+
+Syntax
+
+lambda arguments : expression
+
+------------------------------------------------
+
+Why do we use Lambda Functions?
+
+1. Write short functions.
+2. Avoid creating functions used only once.
+3. Work with map(), filter(), reduce().
+4. Used in sorting.
+5. Common in Data Science and AI/ML.
+
+------------------------------------------------
+
+Difference
+
+Normal Function
+---------------
+Uses def
+
+Lambda Function
+---------------
+Uses lambda
+"""
