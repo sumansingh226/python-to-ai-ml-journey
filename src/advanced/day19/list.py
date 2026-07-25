7bdf1be4bcf641ef90a09b8147b3ffce341e6889
Suman Singh <sumansingh13062000@gmail.com>
2026-07-25 22:25:49 +0530

    List Comprehensions in Python

---
+++ src/advanced/day19/list.py
@@ -0,0 +1,32 @@
+"""
+List Comprehensions in Python
+
+What is a List Comprehension?
+
+A list comprehension is a short and elegant
+way to create a new list using a single line
+of code.
+
+It replaces many traditional for loops.
+
+------------------------------------------------
+
+Why do we use List Comprehensions?
+
+1. Write less code.
+2. Improve readability.
+3. Faster than manual loops.
+4. Transform data easily.
+5. Used heavily in AI/ML.
+
+------------------------------------------------
+
+Syntax
+
+[expression for item in iterable]
+
+With Condition
+
+[expression for item in iterable if condition]
+"""
+
