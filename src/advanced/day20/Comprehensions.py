63e596ee696826d389bcf1fa8cdd18543c6f84f1
Suman Singh <sumansingh13062000@gmail.com>
2026-07-27 19:39:38 +0530

    Comprehensions

--- src/advanced/day20/Comprehensions.py
+++ src/advanced/day20/Comprehensions.py
@@ -55,3 +55,96 @@ print(squares)
 
 print()
 
+
+# Example 3 : Even Numbers
+
+numbers = range(1, 11)
+
+even_numbers = [number for number in numbers if number % 2 == 0]
+
+print(even_numbers)
+
+print()
+
+
+# Example 4 : Odd Numbers
+
+numbers = range(1, 11)
+
+odd_numbers = [number for number in numbers if number % 2 != 0]
+
+print(odd_numbers)
+
+print()
+
+
+# Example 5 : Convert to Uppercase
+
+languages = ["python", "java", "c++"]
+
+uppercase = [language.upper() for language in languages]
+
+print(uppercase)
+
+print()
+
+
+# Example 6 : String Length
+
+words = ["Python", "AI", "Machine Learning"]
+
+lengths = [len(word) for word in words]
+
+print(lengths)
+
+print()
+
+
+# Example 7 : Multiplication Table
+
+table = [number * 5 for number in range(1, 11)]
+
+print(table)
+
+print()
+
+
+# Example 8 : Replace Negative Numbers
+
+numbers = [-5, 10, -8, 20, 15]
+
+positive = [0 if number < 0 else number for number in numbers]
+
+print(positive)
+
+print()
+
+
+# Example 9 : Nested List Comprehension
+
+matrix = [
+
+    [1, 2, 3],
+
+    [4, 5, 6],
+
+    [7, 8, 9]
+
+]
+
+flatten = [number for row in matrix for number in row]
+
+print(flatten)
+
+print()
+
+
+# Example 10 : AI/ML Example
+
+accuracies = [91, 95, 88, 97, 93]
+
+improved = [accuracy + 1 for accuracy in accuracies]
+
+print(improved)
+
+print()
