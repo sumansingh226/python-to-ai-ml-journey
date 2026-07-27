fb824af438fdcd26c4ee83fa2753602bd0e2ed3f
Suman Singh <sumansingh13062000@gmail.com>
2026-07-27 19:39:19 +0530

    Comprehensions

--- src/advanced/day20/Comprehensions.py
+++ src/advanced/day20/Comprehensions.py
@@ -30,3 +30,28 @@ With Condition
 [expression for item in iterable if condition]
 """
 
+
+# Example 1 : Without List Comprehension
+
+numbers = [1, 2, 3, 4, 5]
+
+squares = []
+
+for number in numbers:
+    squares.append(number ** 2)
+
+print(squares)
+
+print()
+
+
+# Example 2 : With List Comprehension
+
+numbers = [1, 2, 3, 4, 5]
+
+squares = [number ** 2 for number in numbers]
+
+print(squares)
+
+print()
+
