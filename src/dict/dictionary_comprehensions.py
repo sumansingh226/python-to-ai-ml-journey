4dfbdd573cc02d827f9826627d2dfd3131d5f60e
Suman Singh <sumansingh13062000@gmail.com>
2026-07-29 21:01:39 +0530

    dicionary comparision

---
+++ src/dict/dictionary_comprehensions.py
@@ -0,0 +1,31 @@
+"""
+Dictionary Comprehensions in Python
+
+What is a Dictionary Comprehension?
+
+A dictionary comprehension is a short
+and Pythonic way to create dictionaries.
+
+It allows us to generate key-value pairs
+using a single line of code.
+
+------------------------------------------------
+
+Why do we use Dictionary Comprehensions?
+
+1. Write shorter code.
+2. Improve readability.
+3. Create dictionaries quickly.
+4. Transform existing dictionaries.
+5. Common in Data Science and AI/ML.
+
+------------------------------------------------
+
+Syntax
+
+{key : value for item in iterable}
+
+With Condition
+
+{key : value for item in iterable if condition}
+"""
