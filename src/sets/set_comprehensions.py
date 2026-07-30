0b8ee9cb549f98eba3abdea35b15ec2b09a4bcc9
Suman Singh <sumansingh13062000@gmail.com>
2026-07-30 22:05:22 +0530

    set comparision

--- src/sets/set_comprehensions.py
+++ src/sets/set_comprehensions.py
@@ -30,7 +30,10 @@ With Condition
 {expression for item in iterable if condition}
 """
 
+
+
 # Example 1 : Normal Way
+
 
 numbers = [1, 2, 3, 4, 5]
 
@@ -45,8 +48,9 @@ print(squares)
 print()
 
 
+
 # Example 2 : Set Comprehension
-# ==========================================
+
 
 numbers = [1, 2, 3, 4, 5]
 
@@ -58,7 +62,9 @@ print(squares)
 print()
 
 
+
 # Example 3 : Remove Duplicates
+
 
 numbers = [1, 2, 2, 3, 3, 4, 5, 5]
 
@@ -70,7 +76,9 @@ print(unique_numbers)
 print()
 
 
+
 # Example 4 : Even Numbers
+
 
 numbers = range(1, 11)
 
@@ -84,3 +92,144 @@ print(even_numbers)
 
 
 print()
+
+
+
+# Example 5 : Odd Numbers
+
+
+numbers = range(1, 11)
+
+odd_numbers = {
+    number
+    for number in numbers
+    if number % 2 != 0
+}
+
+print(odd_numbers)
+
+
+print()
+
+
+
+# Example 6 : Convert Strings
+
+
+languages = [
+    "python",
+    "java",
+    "python",
+    "c++"
+]
+
+
+upper_languages = {
+    language.upper()
+    for language in languages
+}
+
+
+print(upper_languages)
+
+
+print()
+
+
+
+# Example 7 : Length of Words
+
+
+words = [
+    "Python",
+    "AI",
+    "Machine Learning"
+]
+
+
+lengths = {
+    len(word)
+    for word in words
+}
+
+
+print(lengths)
+
+
+print()
+
+
+
+# Example 8 : Filter Values
+
+
+marks = [
+    45,
+    80,
+    90,
+    30,
+    95
+]
+
+
+passed_marks = {
+    mark
+    for mark in marks
+    if mark >= 50
+}
+
+
+print(passed_marks)
+
+
+print()
+
+
+
+# Example 9 : AI/ML Example
+
+
+predictions = [
+    "Cat",
+    "Dog",
+    "Cat",
+    "Bird",
+    "Dog"
+]
+
+
+classes = {
+    prediction
+    for prediction in predictions
+}
+
+
+print(classes)
+
+
+print()
+
+
+
+# Example 10 : Extract Unique Features
+
+
+dataset = [
+
+    ["age", "salary"],
+
+    ["age", "city"],
+
+    ["salary", "experience"]
+
+]
+
+
+features = {
+    feature
+    for row in dataset
+    for feature in row
+}
+
+
+print(features)
