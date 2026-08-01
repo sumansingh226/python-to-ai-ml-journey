5aaac4ef909558524b3a41c67370caaa7345ab09
Suman Singh <sumansingh13062000@gmail.com>
2026-08-01 22:18:06 +0530

    Context Managers (with Statement) in Python

---
+++ src/context/context_managers.py
@@ -0,0 +1,31 @@
+"""
+Context Managers (with Statement) in Python
+
+What is a Context Manager?
+
+A context manager is an object that
+automatically manages resources.
+
+It acquires a resource before a block
+of code executes and releases it after
+the block finishes.
+
+------------------------------------------------
+
+Why do we use Context Managers?
+
+1. Automatically close files.
+2. Prevent resource leaks.
+3. Write cleaner code.
+4. Handle exceptions safely.
+5. Used with files, databases, locks,
+   sockets, and AI/ML resources.
+
+------------------------------------------------
+
+Syntax
+
+with expression as variable:
+    # Code Block
+"""
+
