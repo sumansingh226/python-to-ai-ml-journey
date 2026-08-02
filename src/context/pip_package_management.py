d1878cc79082ef8eee66f97baa576e2ecd963cb9
Suman Singh <sumansingh13062000@gmail.com>
2026-08-02 22:01:48 +0530

    pip

---
+++ src/context/pip_package_management.py
@@ -0,0 +1,56 @@
+"""
+pip & Package Management in Python
+
+What is pip?
+
+pip is Python's package manager.
+
+It allows you to install, upgrade,
+remove, and manage third-party packages.
+
+------------------------------------------------
+
+Why do we use pip?
+
+1. Install external libraries.
+2. Upgrade packages.
+3. Remove packages.
+4. View installed packages.
+5. Manage project dependencies.
+
+------------------------------------------------
+
+Examples of Popular Packages
+
+AI / ML
+-------
+numpy
+pandas
+matplotlib
+scikit-learn
+tensorflow
+torch
+
+Web Development
+---------------
+flask
+django
+fastapi
+
+Database
+--------
+sqlalchemy
+psycopg2
+
+Utilities
+---------
+requests
+python-dotenv
+bcrypt
+
+------------------------------------------------
+
+General Syntax
+
+pip command package_name
+"""
