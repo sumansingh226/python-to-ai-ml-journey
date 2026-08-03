d478b9458bb25ea01a4fabce211a1e14f2826759
Suman Singh <sumansingh13062000@gmail.com>
2026-08-03 21:09:49 +0530

    deal with json file

---
+++ src/context/json.py
@@ -0,0 +1,33 @@
+"""
+JSON in Python
+
+What is JSON?
+
+JSON stands for JavaScript Object Notation.
+
+It is a lightweight data-interchange format
+used to store and exchange data.
+
+JSON is:
+
+- Human-readable
+- Easy to write
+- Easy to parse
+- Language independent
+
+------------------------------------------------
+
+Why do we use JSON?
+
+1. Store structured data.
+2. Exchange data between applications.
+3. Send and receive API responses.
+4. Store configuration files.
+5. Save AI/ML datasets.
+
+------------------------------------------------
+
+Python Module
+
+import json
+"""
