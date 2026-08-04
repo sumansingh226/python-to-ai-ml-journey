f523f87717e0c0020ef102c87fd9d54b234226b5
Suman Singh <sumansingh13062000@gmail.com>
2026-08-04 21:18:25 +0530

    os_module.py

---
+++ src/misc/os_module.py
@@ -0,0 +1,35 @@
+"""
+OS Module in Python
+
+What is the os Module?
+
+The os module provides functions to
+interact with the operating system.
+
+It allows you to:
+
+- Work with files
+- Work with directories
+- Access environment variables
+- Execute operating system commands
+- Handle file paths
+
+------------------------------------------------
+
+Why do we use os Module?
+
+1. Create directories.
+2. Delete files and folders.
+3. Rename files.
+4. Get current working directory.
+5. Check if files exist.
+6. Read environment variables.
+7. Build file paths.
+8. Used in automation and AI/ML projects.
+
+------------------------------------------------
+
+Import
+
+import os
+"""
