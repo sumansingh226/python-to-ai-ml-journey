dc3fe093626842f06451c43283e4088ce493e0e0
Suman Singh <sumansingh13062000@gmail.com>
2026-08-06 20:32:03 +0530

    shutil Module in Python

---
+++ src/misc/shutil.py
@@ -0,0 +1,28 @@
+"""
+shutil Module in Python
+
+What is shutil?
+
+shutil stands for "shell utilities".
+
+It is a built-in Python module used for
+high-level file and directory operations.
+
+------------------------------------------------
+
+Why do we use shutil?
+
+1. Copy files.
+2. Copy complete directories.
+3. Move files/folders.
+4. Delete directories.
+5. Create archives.
+6. Extract archives.
+7. Manage datasets and backups.
+
+------------------------------------------------
+
+Import
+
+import shutil
+"""
