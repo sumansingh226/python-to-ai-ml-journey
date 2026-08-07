b70a6a12bfaeba0f94b7f03492d81547af32ed38
Suman Singh <sumansingh13062000@gmail.com>
2026-08-07 20:29:27 +0530

    datetime Module in Python

---
+++ src/misc/datetime.py
@@ -0,0 +1,34 @@
+"""
+datetime Module in Python
+
+What is datetime?
+
+The datetime module is used to work with
+dates and times.
+
+It allows you to:
+
+- Get the current date and time
+- Create custom dates
+- Format dates
+- Parse date strings
+- Perform date calculations
+
+------------------------------------------------
+
+Why do we use datetime?
+
+1. Store timestamps.
+2. Calculate age.
+3. Measure time differences.
+4. Format dates.
+5. Parse user input.
+6. Log events.
+7. Work with AI/ML datasets.
+
+------------------------------------------------
+
+Import
+
+from datetime import datetime, date, time, timedelta
+"""
