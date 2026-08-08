72a80537a1d61769ceb4ea768af9808d4cd3e9c8
Suman Singh <79319168+sumansingh226@users.noreply.github.com>
2026-08-08 22:14:10 +0530

    Create pathlib1.py

---
+++ src/misc/pathlib1.py
@@ -0,0 +1,33 @@
+"""
+pathlib Module in Python
+
+What is pathlib?
+
+pathlib is a built-in Python module used
+to work with files and directories.
+
+It provides an object-oriented way to
+handle file paths.
+
+------------------------------------------------
+
+Why do we use pathlib?
+
+1. Work with files and directories.
+2. Create folders.
+3. Check whether files exist.
+4. Read and write files.
+5. Build paths easily.
+6. Make code cleaner.
+7. Works across operating systems.
+
+------------------------------------------------
+
+Main Class
+
+Path
+
+Import:
+
+from pathlib import Path
+"""
