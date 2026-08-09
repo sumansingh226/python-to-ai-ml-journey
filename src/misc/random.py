746dce4c0d09665ec3021d9b906e9db8c6372992
Suman Singh <sumansingh13062000@gmail.com>
2026-08-09 22:47:20 +0530

    random Module in Python

---
+++ src/misc/random.py
@@ -0,0 +1,20 @@
+"""
+random Module in Python
+
+The random module is used to generate
+pseudo-random values.
+
+------------------------------------------------
+
+Common Functions
+
+random.random()
+random.randint()
+random.randrange()
+random.uniform()
+random.choice()
+random.choices()
+random.sample()
+random.shuffle()
+random.seed()
+"""
