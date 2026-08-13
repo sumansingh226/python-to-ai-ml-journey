6510186f335a1868f019b4bebeed6b8a1204f58a
Suman Singh <sumansingh13062000@gmail.com>
2026-08-13 21:01:54 +0530

    math

--- src/misc/math.py
+++ src/misc/math.py
@@ -24,3 +24,19 @@ Common constants:
 pi
 e
 """
+import math
+#square root 
+print(math.sqrt(25))
+
+
+# 4. Floor
+
+print(math.floor(4.8))
+
+print()
+
+#factorial
+print(math.factorial(5))
+
+print()
+
