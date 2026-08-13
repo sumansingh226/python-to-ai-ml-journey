1069de74dd67c3aa3fd27fda1946c50ecc71ae2e
Suman Singh <sumansingh13062000@gmail.com>
2026-08-13 20:59:44 +0530

    math

---
+++ src/misc/math.py
@@ -0,0 +1,26 @@
+"""
+math Module in Python
+
+The math module provides mathematical
+functions and constants.
+
+Common functions:
+
+sqrt()
+pow()
+ceil()
+floor()
+factorial()
+fabs()
+gcd()
+log()
+log10()
+sin()
+cos()
+tan()
+
+Common constants:
+
+pi
+e
+"""
