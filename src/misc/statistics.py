d8a252dc398ec8601b74b0942e2d7a35dd2f069b
Suman Singh <sumansingh13062000@gmail.com>
2026-08-14 21:08:54 +0530

    static module in python

--- src/misc/statistics.py
+++ src/misc/statistics.py
@@ -15,3 +15,24 @@ stdev()
 pvariance()
 pstdev()
 """
+
+"""
+statistics Module in Python
+
+The statistics module provides functions
+for working with numerical data.
+
+Important functions:
+
+mean()
+median()
+mode()
+multimode()
+variance()
+stdev()
+pvariance()
+pstdev()
+"""
+
+import statistics
+
