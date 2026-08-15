7107b420fe6e30e2725c620c88437ce6edae1fe7
Suman Singh <79319168+sumansingh226@users.noreply.github.com>
2026-08-15 22:02:16 +0530

    Create regex.py

---
+++ src/misc/regex.py
@@ -0,0 +1,19 @@
+"""
+REGULAR EXPRESSIONS (re)
+
+The re module is used to work with
+patterns inside strings.
+
+Common uses:
+
+1. Search text
+2. Find patterns
+3. Extract information
+4. Validate text
+5. Replace text
+6. Split text
+7. Clean data
+
+Import:
+    import re
+"""
