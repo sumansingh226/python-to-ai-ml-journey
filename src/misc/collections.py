64e94d114929089df64ed44777394947af2c425c
Suman Singh <79319168+sumansingh226@users.noreply.github.com>
2026-08-16 19:32:56 +0530

    Create collections.py

---
+++ src/misc/collections.py
@@ -0,0 +1,17 @@
+"""
+collections Module
+
+The collections module provides specialized
+container data types.
+
+Main tools:
+
+Counter
+defaultdict
+deque
+namedtuple
+OrderedDict
+ChainMap
+
+GA4-style examples are included.
+"""
