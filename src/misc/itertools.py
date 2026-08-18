491b2c026b0c000099afc415e6b3c39ce724dca2
Suman Singh <sumansingh13062000@gmail.com>
2026-08-18 17:45:50 +0530

    itertools in python

--- src/misc/itertools.py
+++ src/misc/itertools.py
@@ -12,3 +12,18 @@ permutations(): Generates all possible orderings of an iterable.
 
 combinations(): Generates all possible combinations of a specific length, where order does not matter.
 """
+
+
+import itertools
+
+# product: Cartesian product (like a nested loop)
+list(itertools.product(['A', 'B'], [1, 2])) 
+# Output: [('A', 1), ('A', 2), ('B', 1), ('B', 2)]
+
+# permutations: Order matters
+list(itertools.permutations([1, 2, 3], 2))
+# Output: [(1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2)]
+
+# combinations: Order doesn't matter
+list(itertools.combinations([1, 2, 3], 2))
+# Output: [(1, 2), (1, 3), (2, 3)]
